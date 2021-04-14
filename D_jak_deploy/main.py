ef61ceee398ff03db10252ebf6381986ea3d4de6
bartooo <bcywinski11@gmail.com>
2021-04-14 18:08:30 +0200

    Add HelloResp class

--- D_jak_deploy/main.py
+++ D_jak_deploy/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 app.counter = 0
+
+class HelloResp(BaseModel):
+    msg: str
 
 @app.get("/")
 def root():
@@ -15,3 +19,7 @@ def hello_name_view(name: str):
 def counter():
     app.counter += 1
     return app.counter
+
+@app.get("/hello/{name}", response_model=HelloResp)
+async def read_item(name: str):
+    return HelloResp(msg=f"Hello {name}")
