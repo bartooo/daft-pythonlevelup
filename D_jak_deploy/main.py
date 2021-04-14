fcd046f07f5511000e8dcacef1386f9ba727b0c7
bartooo <bcywinski11@gmail.com>
2021-04-15 00:52:50 +0200

    Modify displayed message

--- D_jak_deploy/main.py
+++ D_jak_deploy/main.py
@@ -4,17 +4,21 @@ from pydantic import BaseModel
 app = FastAPI()
 app.counter = 0
 
+
 class HelloResp(BaseModel):
     msg: str
 
+
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello world!"}
+
 
 @app.get("/counter")
 def counter():
     app.counter += 1
     return app.counter
+
 
 @app.get("/hello/{name}", response_model=HelloResp)
 async def read_item(name: str):
--- D_jak_deploy/tests.py
+++ D_jak_deploy/tests.py
@@ -9,14 +9,16 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
-    
+    assert response.json() == {"message": "Hello world!"}
+
+
 @pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
 def test_hello_name(name):
     response = client.get(f"/hello/{name}")
     assert response.status_code == 200
-    assert response.text == f'"Hello {name}"'
-    
+    assert response.json() == {"msg": f"Hello {name}"}
+
+
 def test_counter():
     response = client.get(f"/counter")
     assert response.status_code == 200
@@ -25,3 +27,7 @@ def test_counter():
     response = client.get(f"/counter")
     assert response.status_code == 200
     assert response.text == "2"
+    # 3rd Try
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "3"
