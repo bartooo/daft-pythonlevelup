5e00e7f8f72e22816564ab67e8d3c6c081950d17
bartooo <bcywinski11@gmail.com>
2021-04-22 15:16:46 +0200

    Add is_palindrome function

--- decorators/main.py
+++ decorators/main.py
@@ -3,3 +3,14 @@ def greetings(foo):
         return "Hello " + " ".join(i.capitalize() for i in foo(self).split(" "))
 
     return to_capital
+
+
+def is_palindrome(sentence):
+    def check_if_palindrome(self):
+        filtered_s = "".join(filter(str.isalnum, sentence(self)))
+        if filtered_s[::-1].lower() == filtered_s.lower():
+            return sentence(self) + " - is palindrome"
+        else:
+            return sentence(self) + " - is not palindrome"
+
+    return check_if_palindrome
