6430f11a35049e1f2ba90ad3eff9956cae7e03f1
bartooo <b.cywinski@stud.elka.pw.edu.pl>
2021-05-16 12:40:25 +0200

    change CompanyName to optional

--- app/db/crud.py
+++ app/db/crud.py
@@ -67,6 +67,6 @@ def insert_supplier(db: Session, new_supplier: models.Supplier):
 def update_supplier(db: Session, id: int, to_update: schemas.Supplier):
     update_dict = {k: v for k, v in dict(to_update).items() if v is not None}
     db.query(models.Supplier).filter(models.Supplier.SupplierID == id).update(
-        dict(update_dict)
+        update_dict
     )
     db.commit()
